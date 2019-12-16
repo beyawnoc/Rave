1a496e352bd97443a5ef146cbb9ae3dcd433a4d2
Will <will.fleener@fcsamerica.com>
2019-12-16 15:47:34 -0600

    Spawn-able move-able objects prototype

--- Assets/_Scripts/UIManager.cs
+++ Assets/_Scripts/UIManager.cs
@@ -1,11 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public Transform cylinder;
+    public Transform cube;
+    public Transform sphere;
+
     public void createCube()
     {
-        Debug.Log("Button click");
+        Instantiate(cube);
     }
+
+    public void createCylinder()
+    {
+        Instantiate(cylinder);
+    }
+
+    public void createSphere(string test)
+    {
+        Debug.Log(test);
+        Instantiate(sphere);
+    }
+
+    //public void instantiateFurniture(Furniture furniture)
+    //{
+    //    Instantiate()
+    //}
+
+    //public enum Furniture
+    //{
+    //    Sphere_Prefab,
+    //    Cylinder_Prefab,
+    //    Cube_Prefab
+    //}
 }
