0a99dcff943e84261cc8d516e603424e71a6de44
Will <will.fleener@fcsamerica.com>
2019-12-18 11:31:43 -0600

    Slider ui

---
+++ Assets/SliderText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderText : MonoBehaviour
+{
+
+    public Slider slider;
+    public string label;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<Text>().text = label + " " + slider.value.ToString("0.0");
+
+    }
+}
