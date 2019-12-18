fb0e5cfeb529bfe1cf641fb51de9761c3cb77bb4
Will <will.fleener@fcsamerica.com>
2019-12-18 11:04:04 -0600

    Make rotate speed changeable

--- Assets/_Scripts/movable_object.cs
+++ Assets/_Scripts/movable_object.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class movable_object : MonoBehaviour
 {
-
+    public int rotateSpeed = 3;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +21,10 @@ public class movable_object : MonoBehaviour
 
         if (Input.GetKey("q"))
         {
-            transform.Rotate(0,0,5);
+            transform.Rotate(0,0, rotateSpeed);
         } else if (Input.GetKey("e"))
         {
-            transform.Rotate(0, 0, -5);
+            transform.Rotate(0, 0, (rotateSpeed * -1));
 
         }
 
