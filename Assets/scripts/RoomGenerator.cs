c313ecdd995ded1dc1152f7b5d0611d292ab7021
Henry Harrison <Henry.Harrison@fcsamerica.com>
2019-12-18 11:28:20 -0600

    Room generation but good this time

--- Assets/scripts/RoomGenerator.cs
+++ Assets/scripts/RoomGenerator.cs
@@ -21,15 +21,21 @@
 //  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 //  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class PuncturedPlane1 : MonoBehaviour {
+public class RoomGenerator : MonoBehaviour {
 
-    public int roomWidth;
+    public GameObject pointLight;
+    public GameObject ceilingPlane;
 
-    //  width of plane
+    public GameObject floorPlane;
+    // this is z
+    public float length;
+
+    //  width of plane, this is x
 
     [Tooltip("Width of plane")]
     public float width;
@@ -50,31 +56,83 @@ public class PuncturedPlane1 : MonoBehaviour {
     // The holes array contains the rectangular cutout definitions
 
     [Tooltip("Coordinates define the rectangular cutouts ")]
-    public Rect[] holes = new Rect[0];
+    private Rect[] holes = new Rect[0];
 
     private RectInt[] sHoles;
 
-    void Start () {
+    //void Start () {
+    void GenerateRoom(float rHeight, float rWidth, float rLength){
+        height = rHeight;
+        width = rWidth;
+        length = rLength;
 
         // convert dimensions into setpSize units
 
         int sWidth = (int)Mathf.Ceil(width / stepSize);
         int sHeight = (int)Mathf.Ceil(height / stepSize);
+        
 
-        sHoles = new RectInt[holes.Length];
 
-        for (int i = 0; i < holes.Length; i++) {
-            sHoles[i].x = (int)Mathf.Ceil(holes[i].x / stepSize);
-            sHoles[i].y = (int)Mathf.Ceil(holes[i].y / stepSize);
-            sHoles[i].width = (int)Mathf.Ceil(holes[i].width / stepSize);
-            sHoles[i].height = (int)Mathf.Ceil(holes[i].height / stepSize);
-        }
+
+        //sHoles = new RectInt[holes.Length];
+        sHoles = new RectInt[0];
+
+ 
+
+        // for (int i = 0; i < holes.Length; i++) {
+        //     sHoles[i].x = (int)Mathf.Ceil(holes[i].x / stepSize);
+        //     sHoles[i].y = (int)Mathf.Ceil(holes[i].y / stepSize);
+        //     sHoles[i].width = (int)Mathf.Ceil(holes[i].width / stepSize);
+        //     sHoles[i].height = (int)Mathf.Ceil(holes[i].height / stepSize);
+        // }
+
+    //    sHoles = new RectInt[UnityEngine.Random.Range(1, (int) Math.Floor(width / 4) - 1)];
+    //     for (int i = 0; i < sHoles.Length; i++) {
+    //         sHoles[i].x = 1 + (int) Math.Floor(width / 4) * i;
+    //         sHoles[i].y = 1;
+    //         sHoles[i].width = 3;
+    //         sHoles[i].height = (int) height - 2;
+    //     }
 
         //  create plane
 
         GameObject myPlane = createPlane(new Vector3Int(sWidth, sHeight, 3));
-        myPlane.transform.position = new Vector3(0, 0, 2);
-        myPlane.GetComponent<Renderer>().material = planeMaterial;
+        myPlane.transform.position = new Vector3(0 - .5f * width, 0, 0 - .5f * length);
+        Renderer myPlaneRenderer = myPlane.GetComponent<Renderer>();
+        myPlaneRenderer.material = planeMaterial;
+        //myPlaneRenderer.material.mainTextureScale = new Vector2 (width / 20, length / 20);
+
+        //make floor
+        GameObject floor = Instantiate(floorPlane, new Vector3(0, (float)0.01, 0), Quaternion.identity);
+        // Debug.Log(width + " lw1 " + length);
+        // Debug.Log(width / 10.0 + " lw " + length / 10.0);
+        floor.transform.localScale = new Vector3(width / 10.0f, 1, length / 10.0f);
+        Renderer floorRender = floor.GetComponent<Renderer>();
+        floorRender.material.mainTextureScale = new Vector2 (width / 7.5f, length / 7.5f);
+        floorRender.material.color = UnityEngine.Random.ColorHSV(0f, 1f, .25f, .8f, .25f, .8f);
+
+        //make ceiling
+        GameObject ceiling = Instantiate(ceilingPlane, new Vector3(0, height-.01f, 0), Quaternion.identity);
+        ceiling.transform.localScale = new Vector3(width / 10.0f, 1, length / 10.0f);
+        Renderer ceilingRender = ceiling.GetComponent<Renderer>();
+        ceilingRender.material.mainTextureScale = new Vector2 (width / 10, length / 10);
+        ceiling.transform.localRotation *= Quaternion.Euler(0, 0, 180);
+        //floorRender.material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 1f, 1f);
+
+        if(length >= width){
+            for(int i = 0 ; i < length/2; i+=3 ){
+                Instantiate(pointLight, new Vector3(0, height-2, i), Quaternion.identity);
+                Instantiate(pointLight, new Vector3(0, height-2, -i), Quaternion.identity);
+            }
+        } else {
+            for(int i = 0 ; i < width/2; i+=3 ){
+                Instantiate(pointLight, new Vector3(i, height-2, 0), Quaternion.identity);
+                Instantiate(pointLight, new Vector3(-i, height-2, 0), Quaternion.identity);
+            }
+        }
+        
+        
+
     }
 
     void Update () {
@@ -95,12 +153,12 @@ public class PuncturedPlane1 : MonoBehaviour {
         Mesh hwall2 = (Mesh)Instantiate (hwall1);
         Vector3[] newvert = (Vector3[]) hwall2.vertices.Clone();
         for (int i = 0; i < hwall2.vertexCount; i++) {
-            newvert[i] = new Vector3 (hwall2.vertices[i].x, hwall2.vertices[i].y, hwall2.vertices[i].z + roomWidth);
+            newvert[i] = new Vector3 (hwall2.vertices[i].x, hwall2.vertices[i].y, hwall2.vertices[i].z + length);
         }
         hwall2.vertices = newvert;
         hwall1 = FlipMesh(hwall1);
 
-        Mesh wwall3 = createMesh(roomWidth, size.y);
+        Mesh wwall3 = createMesh(length, size.y);
         newvert = (Vector3[]) wwall3.vertices.Clone();
         for (int i = 0; i < wwall3.vertexCount; i++) {
             newvert[i] = new Vector3 (wwall3.vertices[i].z, wwall3.vertices[i].y, wwall3.vertices[i].x);
@@ -118,7 +176,7 @@ public class PuncturedPlane1 : MonoBehaviour {
         //wwall4.uv = wwall4.uv.Reverse().ToArray();
         //wwall4.triangles = wwall4.triangles.Reverse().ToArray();
 
-        Mesh floor = createMesh(size.x, roomWidth);
+        Mesh floor = createMesh(size.x, length);
         newvert = (Vector3[]) floor.vertices.Clone();
         for (int i = 0; i < floor.vertexCount; i++) {
             newvert[i] = new Vector3 (floor.vertices[i].x, floor.vertices[i].z, floor.vertices[i].y);
@@ -135,10 +193,24 @@ public class PuncturedPlane1 : MonoBehaviour {
 
         wwall3.RecalculateNormals();
         wwall4.RecalculateNormals();
-        hwall1.RecalculateNormals();
+        // hwall1.RecalculateNormals();
         //hwall2.RecalculateNormals();
         floor.RecalculateNormals();
         ceiling.RecalculateNormals();
+
+        Debug.Log((int) (Math.Floor(width / 4) - 1));
+        //sHoles = new RectInt[UnityEngine.Random.Range(1, (int) Math.Floor(width / 4) - 1)];
+        sHoles = new RectInt[UnityEngine.Random.Range((int) Math.Floor(width / 6) - 1, (int) Math.Floor(width / 4) - 1)];
+        for (int i = 0; i < sHoles.Length; i++) {
+            sHoles[i].x = 1 + (int) Math.Floor(width / 4) * i;
+            //sHoles[i].x = 1 + (int) Math.Floor(width / sHoles.Length) * i;
+            sHoles[i].y = 1;
+            sHoles[i].width = 3;
+            sHoles[i].height = (int) height - 2;
+        }
+        hwall1 = createMesh(size.x, size.y);
+        hwall1 = FlipMesh(hwall1);
+        hwall1.RecalculateNormals();
 
         mf.mesh = CombineMeshes( new List<Mesh> {hwall1, hwall2, wwall3, wwall4, floor, ceiling});
         //mf.mesh.RecalculateNormals();
@@ -151,13 +223,9 @@ public class PuncturedPlane1 : MonoBehaviour {
         var triangleCount = indices.Length / 3;
         for(var i = 0; i < triangleCount; i++)
         {
-            Debug.Log(indices[i*3]);
-            Debug.Log(indices[i*3+1]);
             var tmp = indices[i*3];
             indices[i*3] = indices[i*3 + 1];
             indices[i*3 + 1] = tmp;
-            Debug.Log(indices[i*3]);
-            Debug.Log(indices[i*3+1]);
         }
         mesh.triangles = indices;
         // additionally flip the vertex normals to get the correct lighting
